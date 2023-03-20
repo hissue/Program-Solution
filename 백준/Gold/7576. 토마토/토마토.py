94bd349524cb4a3f1803ac77cbfbfff63246ddcf
swssue <126448936+swssue@users.noreply.github.com>
2023-03-20 17:39:07 +0900

    [Gold V] Title: 토마토, Time: 2540 ms, Memory: 107228 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -15,9 +15,6 @@ noTomato = 0
 # 토마토가 전체 익었을 때 걸리는 기간
 result = 0
 
-# 남은 토마토의 개수
-restTomato = 0
-
 # 토마토의 익은 날짜
 day = [[0 for _ in range(M)] for _ in range(N)]
 
@@ -29,7 +26,7 @@ for i in range(N):
     for j in range(M):
         if box[i][j] == '1': # 현재 안익은 토마토 인덱스
             que.append((i,j))
-        if box[i][j] != '0': # noTomato의 개수가  N*M의 개수와 동일하면 초기에 모든 토마토가 익어있는 상태
+        if box[i][j] == '0': # noTomato의 개수가  N*M의 개수와 동일하면 초기에 모든 토마토가 익어있는 상태
             noTomato +=1
 # 상하좌우
 dx = [-1,1,0,0]
@@ -46,17 +43,9 @@ while que:
         if 0<=nx<N and 0<=ny<M and box[nx][ny] == '0':
             box[nx][ny] = '1'
             day[nx][ny] = day[x][y] +1
+            noTomato-=1
             que.append((nx,ny))
 
             result = max(result,day[nx][ny])
 
-# 모든 시간이 지나고 토마토 내 0이 있는지 확인
-for i in range(N):
-    restTomato += box[i].count('0')
-
-if noTomato == N*M:
-    print(0)
-elif restTomato > 0:
-    print(-1)
-else:
-    print(result)
+print(result if noTomato==0 else -1)
