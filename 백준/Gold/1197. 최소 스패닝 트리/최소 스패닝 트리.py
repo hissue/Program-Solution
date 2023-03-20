a0c261534f2a160a907af2aab1ebd76d7bdf8a9d
swssue <126448936+swssue@users.noreply.github.com>
2023-03-20 22:24:07 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 276 ms, Memory: 49356 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,29 +1,41 @@
+# 크루스칼
 import sys
-input = sys.stdin.readline
- 
-V, E = map(int, input().split())
-Vroot = [i for i in range(V+1)] # 부모 루트 초기 선언(자기 자신)
-Elist = []
-for _ in range(E):
-    Elist.append(list(map(int, input().split())))
 
-Elist.sort(key=lambda x: x[2])
- 
+# 입력
+V,E = map(int,sys.stdin.readline().split())
+tree = []
+for _ in range(E):
+    a,b,w = map(int,sys.stdin.readline().split())
+    tree.append((a,b,w))
+
+# 가중치 기준으로 정렬
+tree.sort(key=lambda x : x[2])
+
+# 노드의 루트 정보
+pRoot = [ _ for _ in range(V+1)]
+
+# 가중치의 합
+result = 0
+
 def find(x):
-    if x != Vroot[x]:
-        Vroot[x] = find(Vroot[x])
-    return Vroot[x]
- 
- 
-answer = 0
-for s, e, w in Elist:
-    sRoot = find(s)
-    eRoot = find(e)
-    if sRoot != eRoot:
-        if sRoot > eRoot:
-            Vroot[sRoot] = eRoot
+    if x != pRoot[x]:
+        pRoot[x] = find(pRoot[x]) # 부모 루트 찾기
+    return pRoot[x] # 본인의 부모 루트값 출력
+
+def union(a,b,w):
+    global result
+    aRoot = find(a)
+    bRoot = find(b)
+
+    if aRoot != bRoot:
+        if aRoot > bRoot:
+            pRoot[aRoot] = bRoot
         else:
-            Vroot[eRoot] = sRoot
-        answer += w
- 
-print(answer)
+            pRoot[bRoot] = aRoot
+
+        result+=w
+
+for a,b,w in tree:
+    union(a,b,w)
+
+print(result)
