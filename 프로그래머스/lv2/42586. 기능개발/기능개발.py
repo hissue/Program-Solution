ed18baba2d96aa3bf0479a9c726887ad2c308aba
swssue <126448936+swssue@users.noreply.github.com>
2023-04-23 14:25:29 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -0,0 +1,21 @@
+from collections import deque
+import math
+def solution(progresses, speeds):
+    answer = []
+    nocomplete = deque()
+
+    for i in range(len(speeds)):
+        nocomplete.append(math.ceil((100-progresses[i])/speeds[i]))
+    
+    a = nocomplete.popleft()
+    cnt = 1
+    while nocomplete:
+        if nocomplete[0] > a:
+            answer.append(cnt)
+            a = nocomplete.popleft()
+            cnt=1
+        else:
+            nocomplete.popleft()
+            cnt+=1
+    answer.append(cnt) 
+    return answer
