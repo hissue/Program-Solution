727c6b82cbb53cc574e7169ca9ed455a149ba16c
swssue <126448936+swssue@users.noreply.github.com>
2023-04-26 13:49:33 +0900

    [Silver II] Title: 스택 수열, Time: 160 ms, Memory: 41388 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,21 +1,20 @@
 import sys
-
 N = int(sys.stdin.readline())
-arrs = [int(sys.stdin.readline()) for _ in range(N)]
+arr = [int(sys.stdin.readline()) for _ in range(N)]
 stack = []
 result = []
-cnt = 0
-for data in range(1,N+1):
-    stack.append(data)
-    result.append('+')
+cnt=0
+
+for i in range(1,N+1):
+    stack.append(i)
+    result.append('+')    
     
-    while stack and stack[-1] == arrs[cnt]:
+    while stack and stack[-1] == arr[cnt]:
         stack.pop()
         result.append('-')
         cnt+=1
-    
+        
 if stack:
-    print('NO')
+    print("NO")
 else:
-    for i in result:
-        print(i)
+    print(*result,sep='\n')
