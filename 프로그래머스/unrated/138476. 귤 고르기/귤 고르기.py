ba3dc42f7d262c28486e9e1ca31276ddb99a4584
swssue <126448936+swssue@users.noreply.github.com>
2023-05-11 10:34:11 +0900

    [unrated] Title: 귤 고르기, Time: 172.26 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,16 +1,21 @@
-from collections import Counter
 def solution(k, tangerine):
-    answer = 0
-    result = k
-    data = list(zip(Counter(tangerine).keys(),Counter(tangerine).values()))
-    data.sort(key=lambda x : -x[1])
-    
-    for k,v in data:     
-        if result <= 0:
-            break
-            
-        result-=v
-        answer+=1
+    answer = []
+    # 크기 중복 귤 구하기
+    box = {}
+    for i in tangerine:
+        if i in box:
+            box[i] += 1
+        else:
+            box[i] = 1
 
+    # 재정렬
+    for j in box.keys():
+        answer.append(box[j])
+    answer.sort(reverse=True)
 
-    return answer
+    # count
+    cnt = 0
+    while k > 0:
+        k -= answer.pop(0)
+        cnt += 1
+    return cnt
