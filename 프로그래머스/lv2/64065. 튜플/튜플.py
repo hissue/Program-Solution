08ddbd46f395303d8cca7e35c888978287f148d1
swssue <126448936+swssue@users.noreply.github.com>
2023-05-27 10:40:00 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -1,30 +1,15 @@
 def solution(s):
-    str_tuples = s[1:-1]
-    idx = 0
-    temp = ''
-    data = []
-    for st in str_tuples:
-        if st == '{':
-            continue
-        elif st == '}':
-            if temp[0] == ',':
-                temp = temp[1:]
-            data.append(temp.split(','))
-            idx +=1
-            temp =''
-        else:
-            temp += st
+    answer = []
+    s1 = s.lstrip('{').rstrip('}').split('},{')
+    for data in s1:
+        answer.append(data.split(','))
 
-    
-    data.sort(key=lambda x : len(x))
-    answer = []  
+    answer.sort(key=lambda x : len(x))
 
-    for x in data:
-        for d in x:
-            temp = int(d)
-            if temp not in answer:
-                answer.append(temp)
-            
-
-    return answer
-
+    result = []
+    for x in answer:
+        for y in x:
+            temp = int(y)
+            if temp not in result:
+                result.append(temp)
+    return result
