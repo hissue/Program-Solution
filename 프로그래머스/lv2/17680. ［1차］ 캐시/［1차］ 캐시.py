ecba1f93133c51d0ccf2d1ba7703a209d3115ca6
swssue <126448936+swssue@users.noreply.github.com>
2023-05-31 10:13:28 +0900

    [level 2] Title: [1차] 캐시, Time: 0.70 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,22 @@
+
+def solution(cacheSize, cities):
+    answer = 0
+    data = []
+    # cacheSize가 0인 경우
+    if cacheSize == 0:
+        return 5 * len(cities)
+    
+    # cacheSize가 0이 아닌 경우
+    for city in cities:
+        city = city.lower()
+        if city not in data:
+            if len(data) == cacheSize:
+                data.pop(0)
+            answer+=5
+            data.append(city)
+        else:
+            data.remove(city)
+            answer+=1
+            data.append(city)
+
+    return answer
