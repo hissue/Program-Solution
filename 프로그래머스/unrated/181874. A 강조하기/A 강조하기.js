ac3ef2b2364cd36000fa7a9eac8244b4092ef1e9
swssue <126448936+swssue@users.noreply.github.com>
2023-07-20 14:08:33 +0900

    [unrated] Title: A 강조하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -1,3 +1,4 @@
 function solution(myString) {
-    return myString.split('').map((e)=> e === 'a' ? 'A' : e != 'A' ? e.toLowerCase() : e).join('');
+    // return myString.split('').map((e)=> e === 'a' ? 'A' : e != 'A' ? e.toLowerCase() : e).join('');
+    return myString.toLowerCase().replaceAll('a','A')
 }
