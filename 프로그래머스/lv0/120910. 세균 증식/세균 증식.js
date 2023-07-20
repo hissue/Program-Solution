aa100f8dd90a3590f73252caad20e38dceae387c
swssue <126448936+swssue@users.noreply.github.com>
2023-07-20 16:35:24 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
@@ -1,6 +1,6 @@
 function solution(n, t) {
-    for (let time = 0; time < t; time++){
-        n*=2
-    }
-    return n;
+    // for (let time = 0; time < t; time++){
+    //     n*=2
+    // }
+    return n<<t;
 }
