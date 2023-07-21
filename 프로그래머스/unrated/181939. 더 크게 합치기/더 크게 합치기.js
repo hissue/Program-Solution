30e37bf4eab24224b0169db32db4277a78023132
swssue <126448936+swssue@users.noreply.github.com>
2023-07-21 16:50:29 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,5 +1,3 @@
 function solution(a, b) {
-    let strA = a+'';
-    let strB = b+'';
-    return Number(strA+strB) > Number(strB+strA) ? Number(strA+strB) : Number(strB+strA);
+    return Math.max(Number(`${a}${b}`),Number(`${b}${a}`))
 }
