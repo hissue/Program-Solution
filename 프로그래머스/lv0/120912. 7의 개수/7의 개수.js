fd8e64e32a6018736c36f8155e58ec95e62c8d87
swssue <126448936+swssue@users.noreply.github.com>
2023-07-25 22:46:52 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,7 +1,8 @@
 function solution(array) {
-    let answer = '';
-    array.map(e=>{
-        answer+=e;
-    })
-    return answer.length - answer.split('7').join('').length;
+    // let answer = '';
+    // array.map(e=>{
+    //     answer+=e;
+    // })
+    // return answer.length - answer.split('7').join('').length;
+    return array.join('').split('7').length -1 ;
 }
