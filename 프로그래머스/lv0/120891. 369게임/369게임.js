9b65df07f05d8379b8fad4d7e6c726f2db677a4d
swssue <126448936+swssue@users.noreply.github.com>
2023-07-25 19:12:45 +0900

    [level 0] Title: 369게임, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,10 +1,11 @@
 function solution(order) {
-    let temp = String(order).split('');
-    let answer = 0;
-    for (let i=0; i<temp.length; i++){
-        if (temp[i]==='3' || temp[i]==='6' || temp[i]==='9'){
-            answer++;
-        }
-    }
-    return answer;
+    // let temp = String(order).split('');
+    // let answer = 0;
+    // for (let i=0; i<temp.length; i++){
+    //     if (temp[i]==='3' || temp[i]==='6' || temp[i]==='9'){
+    //         answer++;
+    //     }
+    // }
+    // return answer;
+    return (''+order).split(/[369]/).length-1;
 }
