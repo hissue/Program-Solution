d59ed9b85ce89a677aadd88aa63918af9d6432f2
swssue <126448936+swssue@users.noreply.github.com>
2023-07-25 22:21:11 +0900

    [level 0] Title: A로 B 만들기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,7 +1,8 @@
 function solution(before, after) { 
-    let temp = before;
-    for (let i = 0; i < after.length; i++) {
-        temp = temp.replace(after[i], '');
-    }
-    return temp.length ? 0 : 1;
+    // let temp = before;
+    // for (let i = 0; i < after.length; i++) {
+    //     temp = temp.replace(after[i], '');
+    // }
+    // return temp.length ? 0 : 1;
+    return before.split('').sort().join('') === after.split('').sort().join('') ? 1 : 0;
 }
