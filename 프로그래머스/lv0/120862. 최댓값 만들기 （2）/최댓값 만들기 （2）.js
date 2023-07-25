76cb7566c20293f518016c59bc78865b1d637752
swssue <126448936+swssue@users.noreply.github.com>
2023-07-25 18:14:30 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,11 +1,12 @@
 function solution(numbers) {
-    let max_num = -100000000;
-    for (let i=0; i<numbers.length; i++){
-        for (let j=i+1; j<numbers.length;j++){
-            if (max_num < numbers[i]*numbers[j]){
-                max_num = numbers[i]*numbers[j];
-            }
-        }
-    }
-    return max_num;
+    // let max_num = -100000000;
+    // for (let i=0; i<numbers.length; i++){
+    //     for (let j=i+1; j<numbers.length;j++){
+    //         if (max_num < numbers[i]*numbers[j]){
+    //             max_num = numbers[i]*numbers[j];
+    //         }
+    //     }
+    // }
+    numbers.sort((a,b)=>a-b);
+    return Math.max(numbers[0]*numbers[1],numbers[numbers.length-1]*numbers[numbers.length-2])
 }
