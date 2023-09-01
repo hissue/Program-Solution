b40e9e6cbe5fdf8fee513cd64bc96898fe77f042
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-01 22:56:35 +0900

    [unrated] Title: 할 일 목록, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -1,9 +1,10 @@
 function solution(todo_list, finished) {
-    var answer = [];
-    for (let i in todo_list) {
-        if (!finished[i]){
-            answer.push(todo_list[i])
-        }
-    }
-    return answer;
+    // var answer = [];
+    // for (let i in todo_list) {
+    //     if (!finished[i]){
+    //         answer.push(todo_list[i])
+    //     }
+    // }
+    // return answer;
+    return todo_list.filter((e,i) => !finished[i]);
 }
