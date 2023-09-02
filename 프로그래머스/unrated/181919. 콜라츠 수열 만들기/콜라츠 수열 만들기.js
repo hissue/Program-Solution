1b83c47067f3c95b1a87de20ccf1d076dbfb1dbb
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-02 11:35:55 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,8 +1,14 @@
-function solution(n) {
-    let answer = [n];
-    while(n!==1){
-        n = n%2 ? n*3+1 : n/2;
-        answer.push(n);
-    }
-    return answer;
+function solution(n, arr = []) {
+    // let answer = [n];
+    // while(n!==1){
+    //     n = n%2 ? n*3+1 : n/2;
+    //     answer.push(n);
+    // }
+    // return answer;
+    
+    // 재귀
+    arr.push(n);
+    if (n===1) return arr;
+    if (n%2) return solution(n*3+1,arr); 
+    return solution(n/2,arr);
 }
