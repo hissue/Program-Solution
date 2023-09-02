41d880b62517f6a34db91228cced6e7e01a384af
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-02 11:04:03 +0900

    [unrated] Title: 두 수의 연산값 비교하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -1,5 +1,6 @@
 function solution(a, b) {
-    let x = Number(String(a)+String(b));
+    // let x = Number(String(a)+String(b));
+    let x = parseInt(a+""+b+"");
     let y = (2*a*b);
     return x > y ? x : y;
 }
