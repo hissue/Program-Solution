142da701aa99c5ea05436362ee41528e5e396973
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-02 10:55:23 +0900

    [unrated] Title: ad 제거하기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.js
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.js
@@ -1,9 +1,11 @@
 function solution(strArr) {
-    let answer = [];
-    for (let i in strArr){
-        if (!strArr[i].includes("ad")){
-            answer.push(strArr[i]);
-        }
-    }
+    // let answer = [];
+    // for (let i in strArr){
+    //     if (!strArr[i].includes("ad")){
+    //         answer.push(strArr[i]);
+    //     }
+    // }
+    // return answer;
+    let answer = strArr.filter((v)=> !v.includes("ad"))
     return answer;
 }
