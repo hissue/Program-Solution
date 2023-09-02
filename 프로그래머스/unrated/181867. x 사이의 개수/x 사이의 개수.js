7503a008fbebe05d1a7f109e53deb4c672a33219
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-02 10:49:54 +0900

    [unrated] Title: x 사이의 개수, Time: 1.34 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.js
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.js
@@ -1,8 +1,9 @@
 function solution(myString) {
-    let answer = [];
-    let myStringSplit = myString.split('x');
-    for (let i in myStringSplit){
-        answer.push(myStringSplit[i].length);
-    }
-    return answer;
+    // let answer = [];
+    // let myStringSplit = myString.split('x');
+    // for (let i in myStringSplit){
+    //     answer.push(myStringSplit[i].length);
+    // }
+    // return answer;
+    return myString.split('x').map((v) => v.length)
 }
