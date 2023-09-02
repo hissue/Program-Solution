9408bd302d985711170bccebaf09eb53ba80b4b8
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-02 11:56:52 +0900

    [unrated] Title: 배열 만들기 3, Time: 0.47 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,11 +1,13 @@
 function solution(arr, intervals) {
-    let [x,y] = intervals;
-    let answer = [];
-    for (let i=x[0]; i<=x[1]; i++){
-        answer.push(arr[i]);
-    }
-    for (let i=y[0]; i<=y[1]; i++){
-        answer.push(arr[i]);
-    }
-    return answer;
+    // let [x,y] = intervals;
+    // let answer = [];
+    // for (let i=x[0]; i<=x[1]; i++){
+    //     answer.push(arr[i]);
+    // }
+    // for (let i=y[0]; i<=y[1]; i++){
+    //     answer.push(arr[i]);
+    // }
+    // return answer;
+    let [[a,b],[c,d]] = intervals;
+    return [...arr.slice(a,b+1), ...arr.slice(c,d+1)];
 }
