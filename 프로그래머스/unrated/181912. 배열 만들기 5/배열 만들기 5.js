f87bba6ad168093d297d33eaa9dcce7c3310b637
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-03 13:33:06 +0900

    [unrated] Title: 배열 만들기 5, Time: 1.17 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
@@ -1,8 +1,9 @@
 function solution(intStrs, k, s, l) {
-    let answer = [];
-    intStrs.map((v) => {
-        let temp = parseInt(v.split("").splice(s,l).join(""));
-        if (temp > k) answer.push(temp);
-    })
-    return answer;
+    // let answer = [];
+    // intStrs.map((v) => {
+    //     let temp = parseInt(v.split("").splice(s,l).join(""));
+    //     if (temp > k) answer.push(temp);
+    // })
+    // return answer;
+    return intStrs.map((v) => +v.slice(s,s+l)).filter((v) => v > k);
 }
