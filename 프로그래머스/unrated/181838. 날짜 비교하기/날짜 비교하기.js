1b602c4e4bf12464fc97d75983604018eea97474
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-03 12:50:42 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.18 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,8 +1,10 @@
 function solution(date1, date2) {
-    let flag = 0;
-    for (let i in date1){
-        if (date1[i] < date2[i]) return 1;
-        else if(date1[i] > date2[i]) return 0;
-    }
-    return 0;
+    // let flag = 0;
+    // for (let i in date1){
+    //     if (date1[i] < date2[i]) return 1;
+    //     else if(date1[i] > date2[i]) return 0;
+    // }
+    // return 0;
+    
+    return new Date(date1) < new Date(date2) ? 1 : 0;
 }
