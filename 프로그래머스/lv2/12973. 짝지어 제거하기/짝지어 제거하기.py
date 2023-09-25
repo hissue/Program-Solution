cf52499c9c3a3ff43bb7edfba98f3d86cecd7ce6
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-25 20:24:40 +0900

    [level 2] Title: 짝지어 제거하기, Time: 78.66 ms, Memory: 15.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,12 +1,20 @@
-from collections import deque
-def solution(s):
-    left_list = [s[0]]
-    right_list = deque(s[1:])
+# from collections import deque
+# def solution(s):
+#     left_list = [s[0]]
+#     right_list = deque(s[1:])
     
-    while right_list :
-        if not left_list or left_list[-1] != right_list[0]:
-            left_list.append(right_list.popleft())
-        else:  
-            left_list.pop()
-            right_list.popleft()  
-    return 0 if len(left_list) else 1
+#     while right_list :
+#         if not left_list or left_list[-1] != right_list[0]:
+#             left_list.append(right_list.popleft())
+#         else:  
+#             left_list.pop()
+#             right_list.popleft()  
+#     return 0 if len(left_list) else 1
+def solution(s):
+    temp = [" ", s[0]]
+    for i in s[1:]:
+        if temp[-1] == i:
+            temp.pop()
+        else:
+            temp.append(i)
+    return 1 if len(temp) == 1 else 0
