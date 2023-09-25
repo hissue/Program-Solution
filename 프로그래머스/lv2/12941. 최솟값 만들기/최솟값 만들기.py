ec8d38cbf7def1e79a9e79fc2a655d2ccdb6af52
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-25 17:53:00 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.25 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,10 +1,13 @@
-def solution(A,B):
-    answer = 0
+# def solution(A,B):
+#     answer = 0
 
-    A.sort()
-    B.sort()
-    for i in range(len(A)):
-        temp = A.pop(0) * B.pop()
-        print(temp)
-        answer += temp
-    return answer
+#     A.sort()
+#     B.sort()
+#     for i in range(len(A)):
+#         temp = A.pop(0) * B.pop()
+#         print(temp)
+#         answer += temp
+#     return answer
+
+def solution(A,B):
+    return sum([a*b for a,b in zip(sorted(A), sorted(B, reverse=True))])
