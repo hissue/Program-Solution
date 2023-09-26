18e430fb16472c813d33cb4d49d95f2292170cdf
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-26 18:57:27 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -1,19 +1,20 @@
 def solution(a, b):
     months = [0,31,29,31,30,31,30,31,31,30,31,30,31]
-    answer = (sum(months[:a])+b)%7
-    if answer == 1:
-        return "FRI"
-    elif answer == 2:
-        return "SAT"
-    elif answer == 3:
-        return "SUN"
-    elif answer == 4:
-        return "MON"
-    elif answer == 5:
-        return "TUE"
-    elif answer == 6:
-        return "WED"
-    else:
-        return "THU"
+    # answer = (sum(months[:a])+b)%7
+    # if answer == 1:
+    #     return "FRI"
+    # elif answer == 2:
+    #     return "SAT"
+    # elif answer == 3:
+    #     return "SUN"
+    # elif answer == 4:
+    #     return "MON"
+    # elif answer == 5:
+    #     return "TUE"
+    # elif answer == 6:
+    #     return "WED"
+    # else:
+    #     return "THU"
         
-    return answer
+    week = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
+    return week[(sum(months[:a])+b)%7]
