20e83bf30b169843b67ae6e4695438c9798f3913
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-27 14:18:07 +0900

    [level 1] Title: 폰켓몬, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,13 +1,14 @@
 def solution(nums):
-    answer = {}
-    count = 0
-    for data in nums:
-        if data in answer:
-            answer[data] +=1
-        else:
-            answer[data] = 1
-    for k in answer.keys():
-        if count == len(nums)//2:
-            break
-        count+=1
-    return count
+    # answer = {}
+    # count = 0
+    # for data in nums:
+    #     if data in answer:
+    #         answer[data] +=1
+    #     else:
+    #         answer[data] = 1
+    # for k in answer.keys():
+    #     if count == len(nums)//2:
+    #         break
+    #     count+=1
+    # return count
+    return min(len(nums)//2, len(set(nums)))
