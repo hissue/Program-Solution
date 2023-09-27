232be4f34b4a8539511cb7c454281ec82c08f65d
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-27 14:01:57 +0900

    [unrated] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -1,10 +1,9 @@
 def solution(cards1, cards2, goal):
-    length = len(goal)
-    for word in range(length):
-        if cards1 and goal[0] == cards1[0]:
-            goal.pop(0)
+    for word in goal:
+        if cards1 and word == cards1[0]:
             cards1.pop(0)
-        elif cards2 and goal[0] == cards2[0]:
-            goal.pop(0)
+        elif cards2 and word == cards2[0]:
             cards2.pop(0)
-    return "Yes" if not goal else "No"
+        else:
+            return "No"
+    return "Yes"
