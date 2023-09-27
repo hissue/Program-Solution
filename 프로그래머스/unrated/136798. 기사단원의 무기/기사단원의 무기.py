4615f18fed9568fafd57eb38c9359d2786e1c764
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-27 20:16:24 +0900

    [unrated] Title: 기사단원의 무기, Time: 3.46 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,18 +1,21 @@
-def get_divisor(n):
-    data = set()
-
-    for i in range(1, int(n ** (1 / 2)) + 1):
-        if n % i == 0:
-            data.add(i)
-            data.add(n // i)
-    return len(data)
-
 def solution(number, limit, power):
     answer = 0
-    for i in range(1,number+1):
-        result = get_divisor(i)
+    for n in range(1,number+1):
+        data = set()
+
+        for i in range(1, int(n ** (1 / 2)) + 1):
+            if n % i == 0:
+                data.add(i)
+                data.add(n // i)
+                
+            if len(data) > limit:
+                break
+                
+        result = len(data)
+        
         if result > limit:
             answer+=power
         else:
             answer+=result
+            
     return answer
