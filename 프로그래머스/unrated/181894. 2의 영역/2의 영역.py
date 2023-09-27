18850bf07be0c252485fe669a5be27d6f01766de
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-27 13:02:24 +0900

    [unrated] Title: 2의 영역, Time: 0.58 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
@@ -1,15 +1,10 @@
 def solution(arr):
-    start = 0
     end = 0
     if 2 not in arr:
         return [-1]
     else:
-        for idx in range(len(arr)):
-            if arr[idx] == 2:
-                start = idx
-                break
         for idx in range(len(arr)-1,-1,-1):
             if arr[idx] == 2:
                 end = idx
                 break
-        return arr[start:end+1]
+        return arr[arr.index(2):len(arr)-arr[::-1].index(2)]
