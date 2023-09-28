15b9ac3ddad9e186c1978410cae874653b17f513
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-28 15:38:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.73 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,17 @@
+import collections
 def solution(participant, completion):
-    participants = {}
-    for person in participant:
-        if person in participants:
-            participants[person] +=1
-        else:
-            participants[person] =1
+#     participants = {}
+#     for person in participant:
+#         if person in participants:
+#             participants[person] +=1
+#         else:
+#             participants[person] =1
     
-    for comPerson in completion:
-        participants[comPerson]-=1
+#     for comPerson in completion:
+#         participants[comPerson]-=1
 
-    for person in participant:
-        if participants[person]:
-            return person
+#     for person in participant:
+#         if participants[person]:
+#             return person
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
