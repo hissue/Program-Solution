b0d125e3abe4f592d391a74cf6e27346eafd8945
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-29 17:25:34 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -0,0 +1,22 @@
+def solution(id_list, report, k):
+    answer = {}
+    result = {}
+    total = [0] * len(id_list)
+    for data in report:
+        a,b = data.split(" ")
+        if b not in result:
+            result[b] = 0
+            
+        if a not in answer:
+            answer[a] = [b]
+            result[b] +=1  
+        else:
+            if b not in answer[a]:   
+                answer[a].append(b)
+                result[b] +=1
+        
+    for myKey, datas in answer.items():
+        for data in datas:
+            if result[data] >= k:
+                total[id_list.index(myKey)] +=1
+    return total
