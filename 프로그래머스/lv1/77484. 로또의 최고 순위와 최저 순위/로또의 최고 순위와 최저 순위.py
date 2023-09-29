07c1c8b7e39bb3278dfeafde2ed26503b3f0fe3a
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-09-29 16:57:45 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,25 +1,9 @@
-import collections
 def solution(lottos, win_nums):
+    answer = [6,6,5,4,3,2,1]
     zero = lottos.count(0)
     count = 0
-    result = []
     for data in win_nums:
         if data in lottos:
-            lottos.remove(data)
             count+=1
             
-    for data in [zero+count,count]:
-        if data == 6:
-            result.append(1)        
-        elif data == 5:
-            result.append(2)        
-        elif data == 4:
-            result.append(3)        
-        elif data == 3:
-            result.append(4)        
-        elif data == 2:
-            result.append(5)        
-        else:
-            result.append(6)
-    
-    return result
+    return [answer[zero+count],answer[count]]
