fdee599f9baf99e91ea459cef0773bc16df319ba
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-10-02 16:32:25 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.17 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,14 +1,14 @@
 def solution(wallpaper):
-    index = []
-    dic = {}
+    a = []
+    b = []
     for i in range(len(wallpaper)):
         for j in range(len(wallpaper[i])):
             if wallpaper[i][j] == "#":
-                index.append([i,j])
+                a.append(i)
+                b.append(j)
                 
-    lux = index[0][0]
-    rdx = index[-1][0]+1
-    temp = sorted(index,key=lambda x : x[-1])
-    luy = temp[0][-1]
-    rdy = temp[-1][-1]+1
+    lux = min(a)
+    rdx = max(a)+1
+    luy = min(b)
+    rdy = max(b)+1
     return [lux,luy,rdx,rdy]
